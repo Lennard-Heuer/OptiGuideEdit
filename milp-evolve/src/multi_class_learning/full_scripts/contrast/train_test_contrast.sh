4b25869d3d5fa9032f93d0868e137d6ee57f53da
Lennard-Heuer <125029990+Lennard-Heuer@users.noreply.github.com>
2025-02-25 13:11:33 +0900

    Update train_test_contrast.sh

--- milp-evolve/src/multi_class_learning/full_scripts/contrast/train_test_contrast.sh
+++ milp-evolve/src/multi_class_learning/full_scripts/contrast/train_test_contrast.sh
@@ -2,6 +2,6 @@ export EPOCH=1000
 export DATASET=ours
 export EVAL_EPOCHS=10
 export PRINT_ITERS=10000
-export TEXT_TYPES="'description only'"
+export TEXT_TYPES="description_only"
 python /content/OptiGuideEditOld/milp-evolve/src/multi_class_learning/contrast_train_test.py \
     --epochs $EPOCH --dataset $DATASET --eval_epochs $EVAL_EPOCHS --print_iters $PRINT_ITERS --text_types $TEXT_TYPES
